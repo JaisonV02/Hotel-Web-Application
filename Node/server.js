63c944a99650122ae9131f0d56a0e13657e27460
Jaison <91572221+JaisonV02@users.noreply.github.com>
2023-11-18 20:02:11 +0000

    Changed server.js to use express instead for the server.

--- Node/server.js
+++ Node/server.js
@@ -1,25 +1,21 @@
 // Import modules
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
 // Server port and host
 const port = 8080;
 const host = 'localhost';
 
-// Create the server
-const server = http.createServer(function(req, res) {
-    fs.readFile(path.join(__dirname, '../index.html'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.statusCode = 500;
-            res.end('404 Not Found');
-        } else {
-            res.statusCode = 200;
-            res.end(data);
-        }
-    });
+// Create an Express app
+const app = express();
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../')));
+
+// Define the route for the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Start the server
-server.listen(port, host, () => {console.log('Server running on http://' + host + ':' + port + '/');});
+app.listen(port, host, (req, res) => {console.log('App running on http://' + host + ':' + port + '/');});
